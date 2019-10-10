99c3cc330e5d02fefe9333f06683d415fe0143cc
Chris Bharucha <christopher.k.bharucha-1@ou.edu>
2019-10-10 13:34:19 -0500

    finished RegionTest.java

--- RegionTest.java
+++ RegionTest.java
@@ -15,5 +15,24 @@ public class RegionTest
     public void regionToStringTest() throws AssertException
     {
     	// TODO: implement.
+    	String expected1 = "north";
+    	String actual1 = Region.NORTH.toString();
+    	
+    	Assert.assertEquals(expected1, actual1);
+    	
+    	String expected2 = "south";
+    	String actual2 = Region.SOUTH.toString();
+    	
+    	Assert.assertEquals(expected2, actual2);
+    	
+    	String expected3 = "east";
+    	String actual3 = Region.EAST.toString();
+    	
+    	Assert.assertEquals(expected3, actual3);
+    	
+    	String expected4 = "west";
+    	String actual4 = Region.WEST.toString();
+    	
+    	Assert.assertEquals(expected4, actual4);
     }
 }
