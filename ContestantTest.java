930b39d1c3bbc009a0f02c9e558736a645cf65d4
Chris Bharucha <christopher.k.bharucha-1@ou.edu>
2019-10-10 16:27:06 -0500

    finished Contestant.java and ContestantTest.java

--- ContestantTest.java
+++ ContestantTest.java
@@ -15,6 +15,10 @@ public class ContestantTest
     public void contestantGeorgeGetRegionTest() throws AssertException
     {
     	// TODO: implement.
+    	Region expected = Region.NORTH;
+    	Region actual = Contestant.GEORGE.getRegion();
+    	
+    	Assert.assertEquals(expected, actual);
     }
     
     /**
@@ -23,6 +27,10 @@ public class ContestantTest
     public void contestantJillGetRegionTest() throws AssertException
     {
     	// TODO: implement.
+    	Region expected = Region.SOUTH;
+    	Region actual = Contestant.JILL.getRegion();
+    	
+    	Assert.assertEquals(expected, actual);
     }
 
     /**
@@ -31,6 +39,10 @@ public class ContestantTest
     public void contestantMatthewGetRegionTest() throws AssertException
     {
     	// TODO: implement.
+    	Region expected = Region.EAST;
+    	Region actual = Contestant.MATTHEW.getRegion();
+    	
+    	Assert.assertEquals(expected, actual);
     }
 
     /**
@@ -39,6 +51,10 @@ public class ContestantTest
     public void contestantBettyGetRegionTest() throws AssertException
     {
     	// TODO: implement.
+    	Region expected = Region.WEST;
+    	Region actual = Contestant.BETTY.getRegion();
+    	
+    	Assert.assertEquals(expected, actual);
     }
 
     /**
@@ -47,6 +63,10 @@ public class ContestantTest
     public void contestantGeorgeGetChoiceTest() throws AssertException
     {
     	// TODO: implement.
+    	HandChoice expected = HandChoice.ROCK;
+    	HandChoice actual = Contestant.GEORGE.getChoice();
+    	
+    	Assert.assertEquals(expected, actual);
     }
     
     /**
@@ -56,6 +76,10 @@ public class ContestantTest
     public void contestantJillGetChoiceTest() throws AssertException
     { 
     	// TODO: implement.
+    	HandChoice expected = HandChoice.ROCK;
+    	HandChoice actual = Contestant.JILL.getChoice();
+    	
+    	Assert.assertEquals(expected, actual);
     }
 
     /**
@@ -64,6 +88,10 @@ public class ContestantTest
     public void contestantMatthewGetChoiceTest() throws AssertException
     {
     	// TODO: implement.
+    	HandChoice expected = HandChoice.SCISSORS;
+    	HandChoice actual = Contestant.MATTHEW.getChoice();
+    	
+    	Assert.assertEquals(expected, actual);
     }
 
     /**
@@ -72,6 +100,10 @@ public class ContestantTest
     public void contestantBettyGetChoiceTest() throws AssertException
     {
     	// TODO: implement.
+    	HandChoice expected = HandChoice.PAPER;
+    	HandChoice actual = Contestant.BETTY.getChoice();
+    	
+    	Assert.assertEquals(expected, actual);
     }
 
     /**
@@ -80,5 +112,20 @@ public class ContestantTest
     public void contestantToStringTest() throws AssertException
     {
     	// TODO: implement.
+    	String expected1 = "george: contestant from north throwing rock";
+    	String actual1 = Contestant.GEORGE.toString();
+    	Assert.assertEquals(expected1, actual1);
+    	
+    	String expected2 = "jill: contestant from south throwing rock";
+    	String actual2 = Contestant.JILL.toString();
+    	Assert.assertEquals(expected2, actual2);
+    	
+    	String expected3 = "matthew: contestant from east throwing scissors";
+    	String actual3 = Contestant.MATTHEW.toString();
+    	Assert.assertEquals(expected3, actual3);
+    	
+    	String expected4 = "betty: contestant from west throwing paper";
+    	String actual4 = Contestant.BETTY.toString();
+    	Assert.assertEquals(expected4, actual4);
     }
 }
